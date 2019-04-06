53df691118556b67f69162c1f0dea453ac75bfc6
ryan <rthomma@masonlive.gmu.edu>
2019-04-06 17:30:53 -0400

    #16: Testing call from UIClass.java to Notifications.java.

--- app/src/main/java/RedditAlarm/Notifications.java
+++ app/src/main/java/RedditAlarm/Notifications.java
@@ -1,21 +1,23 @@
 package RedditAlarm;
 //import android.app.NotificationManager;
 import android.app.Notification; // Maybe change to Notification.Builder
+import android.app.NotificationManager;
 import android.content.Context;
 
 
 // NOTE: This class uses Notification.Builder, not Notification
 public class Notifications {
 
-    private Notification.Builder noti;
+    private static Notification noti;
 
     // assume we have context set up, method here will be called with the existing context passed in from LogicHandler
-    public Notification.Builder newNotification(Context context) {
-        noti = new Notification.Builder(context)
+    public static Notification newNotification(Context context) {
+        noti = new Notification()
         .setContentTitle("Notification.")
         .setContentText("This is your notification.")
         .setVisibility(Notification.VISIBILITY_PUBLIC);
         // now methods to display it, maybe include preferences in making this better
+        noti.notify();
         return noti;
     }
 
