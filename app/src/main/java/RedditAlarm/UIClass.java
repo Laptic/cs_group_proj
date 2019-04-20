81b659e88f76f12937e80cf6ad6c0284bc22d7d8
bchesnut <bchesnut@harttech.com>
2019-04-20 17:58:19 -0400

    Cleaning up and edits to alarm calls

--- app/src/main/java/RedditAlarm/UIClass.java
+++ app/src/main/java/RedditAlarm/UIClass.java
@@ -2,22 +2,9 @@ package RedditAlarm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.CompoundButton;
-import android.widget.Spinner;
-import android.widget.TextView;
-import android.widget.ToggleButton;
 import android.support.v4.app.FragmentTransaction;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.PriorityQueue;
-
-import RedditAlarm.Models.RedditJSON;
-import retrofit2.Call;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 
 public class UIClass extends AppCompatActivity implements AlarmFragment.passAlarm {
@@ -32,10 +19,8 @@ public class UIClass extends AppCompatActivity implements AlarmFragment.passAlar
         super.onCreate(savedInstanceState);
         setContentView(R.layout.base_layout);
 
-
         logicReference = new LogicHandler(this);
         listOfAlarms = logicReference.alarmList;
-
 
         // temporary call for Notifications testing
         //Notification.Builder tempNotification = Notifications.newNotification(this);
